f5ffa241c47bf750cd7aea6f2c69ea1d49af6135
Tino Hartmann <tino.hartmann@gmail.com>
2020-12-15 09:53:49 +0100

    msg logging

---
+++ include/algoviz/Canvas.hpp
@@ -0,0 +1,11 @@
+ #ifndef CANVAS_HPP
+ #define CANVAS_HPP
+  
+ #include <iostream>
+ #include <string>
+ #include "xeus/xjson.hpp"
+ #include "nlohmann/json.hpp"
+ #include <algoviz/AlgoViz.hpp>
+
+
+ #endif
